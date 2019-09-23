c673e02922649c0cf6e14f9cfb368eb0de63ba4e
HoangDH <huuhoang.dao@gmail.com>
2019-09-23 23:42:47 +0700

    Initial commit

---
+++ main/background.ts
@@ -0,0 +1,35 @@
+import { app } from 'electron';
+import serve from 'electron-serve';
+import {
+  createWindow,
+  exitOnChange,
+} from './helpers';
+
+const isProd: boolean = process.env.NODE_ENV === 'production';
+
+if (isProd) {
+  serve({ directory: 'app' });
+} else {
+  exitOnChange();
+  app.setPath('userData', `${app.getPath('userData')} (development)`);
+}
+
+(async () => {
+  await app.whenReady();
+
+  const mainWindow = createWindow('main', {
+    width: 1000,
+    height: 600,
+  });
+
+  const homeUrl: string = isProd ? 'app://./home.html' : 'http://localhost:8888/home';
+  await mainWindow.loadURL(homeUrl);
+
+  if (!isProd) {
+    mainWindow.webContents.openDevTools();
+  }
+})();
+
+app.on('window-all-closed', () => {
+  app.quit();
+});
